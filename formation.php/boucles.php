1180b0ff95772864dd744b2e06c73ae0c6951404
Mourtada10 <mourtada@outlook.com>
2026-03-09 16:21:13 +0100

    Boucles

---
+++ formation.php/boucles.php
@@ -0,0 +1,18 @@
+<?php
+$projets = ["site vitrine"];
+$projets[] = "intranet";
+$projets[] = "application mobile";
+
+for($i = 0; $i < 3; $i++){
+    echo $projets[$i] ."<br>";
+}
+
+$j = 0;
+while($j < 3){
+    echo $projets[$j];
+    $j++;
+}
+
+foreach($projets as $projet){
+    echo $projet . "<br>";
+}
